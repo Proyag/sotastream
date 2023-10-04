a76334ad53f9a8cb42a8664708dae04dbb8041c9
Proyag <proyag.pal@ed.ac.uk>
2023-10-04 14:48:45 +0000

    Option to trim field to the last N (chosen at random) tokens (for document context)

--- sotastream/pipelines/sample_from_fields_pipeline.py
+++ sotastream/pipelines/sample_from_fields_pipeline.py
@@ -14,23 +14,22 @@ class SampleFromFieldsPipeline(Pipeline):
     def __init__(self, parallel_data, **kwargs):
         super().__init__(**kwargs)
 
-        self.url_domains = kwargs.get('url_domains', -1)
-        self.sample_fields = kwargs.get('sample_fields', [1])
-        self.delimiter = kwargs.get('delimiter', ',')
-
         self.stream = self.create_data_stream(
             parallel_data,
             processor=partial(
                 ReadAndSample,
-                url_domains=self.url_domains,
-                sample_fields=self.sample_fields,
-                delimiter=self.delimiter,
+                url_domains=kwargs.get('url_domains', -1),
+                sample_fields=kwargs.get('sample_fields', [1]),
+                delimiter=kwargs.get('delimiter', ','),
+                keep_last_n_tokens=kwargs.get('keep_last_n_tokens', -1),
             ),
         )
+        # PPTODO: Add separate sources for document and sentence data, with Mixer
         self.stream = FieldFilter(self.stream, fields=kwargs.get('keep_fields', []))
 
     @classmethod
     def get_data_sources_for_argparse(cls):
+        # PPTODO: Add separate sources for document and sentence data
         return [('parallel_data', 'Path to parallel data (folder with .gz files, or compressed TSV)')]
 
     @classmethod
@@ -48,7 +47,7 @@ class SampleFromFieldsPipeline(Pipeline):
             "--url-domains",
             type=int,
             default=-1,
-            help="Trim URLs to only web domains in field {METAVAR}",
+            help="Trim URLs to only web domains in field (0-indexed)",
         )
         parser.add_argument(
             "--sample-fields",
@@ -64,10 +63,16 @@ class SampleFromFieldsPipeline(Pipeline):
             default=[],
             help="Which fields to keep (0-indexed). Keeps all fields if empty.",
         )
+        parser.add_argument(
+            "--keep-last-n-tokens",
+            type=int,
+            default=-1,
+            help="Which field (0-indexed) to keep only the last N tokens of, where N is sampled from [1, max_n]. -1 to disable. max_n is currently hardcoded to 512.",
+        )
         parser.add_argument("--delimiter", type=str, default=',', help="Delimiter to split list on")
 
 
-def SampleFromFields(stream, url_domains: int = -1, sample_fields: List[int] = [1], delimiter: str = ','):
+def SampleFromFields(stream, sample_fields: List[int] = [1], delimiter: str = ','):
     """
     Randomly samples a single element from a comma-separated list in the third field.
     """
@@ -78,27 +83,56 @@ def SampleFromFields(stream, url_domains: int = -1, sample_fields: List[int] = [
 
             line[sample_field] = random.choice(line[sample_field].split(delimiter))
 
-            if sample_field == url_domains:
-                # Extract web domain from URL
-                if (
-                    line[sample_field].startswith('//')
-                    or line[sample_field].startswith('http://')
-                    or line[sample_field].startswith('https://')
-                ):
-                    line[sample_field] = urlparse(line[sample_field]).netloc
-                else:
-                    # "urlparse recognizes a netloc only if it is properly introduced by ‘//’"
-                    line[sample_field] = urlparse(f"//{line[sample_field]}").netloc
         yield line
 
 
-def ReadAndSample(path: str, url_domains: int = -1, sample_fields: List[int] = [1], delimiter: str = ','):
+def GetURLDomain(stream, url_field: int = -1):
+    """
+    Extracts the domain from a URL field.
+    """
+    for line in stream:
+        if url_field == -1 or url_field >= len(line):
+            yield line
+        else:
+            if (
+                line[url_field].startswith('//')
+                or line[url_field].startswith('http://')
+                or line[url_field].startswith('https://')
+            ):
+                line[url_field] = urlparse(line[url_field]).netloc
+            else:
+                # "urlparse recognizes a netloc only if it is properly introduced by ‘//’"
+                line[url_field] = urlparse(f"//{line[url_field]}").netloc
+            yield line
+
+
+def KeepLastNTokens(stream, field: int = -1, max_n: int = 512):
+    """
+    Sample N from [1, max_n], and keep the last N tokens of the field.
+    """
+    # PPTODO: Be smarter to not split sentences
+    for line in stream:
+        if field == -1 or field >= len(line):
+            yield line
+        else:
+            n = random.randint(1, max_n)
+            line[field] = ' '.join(line[field].split()[-n:])
+        yield line
+
+
+def ReadAndSample(
+    path: str,
+    url_domains: int = -1,
+    sample_fields: List[int] = [1],
+    delimiter: str = ',',
+    keep_last_n_tokens: int = -1,
+):
     """
     Opens a file as a stream and passes it through SampleFromCommaSeparatedList.
     """
     stream = UTF8File(path)
-    stream = SampleFromFields(
-        stream, url_domains=url_domains, sample_fields=sample_fields, delimiter=delimiter
-    )
+    stream = SampleFromFields(stream, sample_fields=sample_fields, delimiter=delimiter)
+    stream = GetURLDomain(stream, url_field=url_domains)
+    stream = KeepLastNTokens(stream, field=keep_last_n_tokens, max_n=512)
 
     return stream
